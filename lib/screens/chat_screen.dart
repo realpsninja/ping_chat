389e1f7c82d7f369ba4caac81a1d0b8e49b4adc0
realpsninja <n1kolaevartemkaa@gmail.com>
2026-01-28 23:17:54 +0000

    Improve UI elements

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -497,7 +497,7 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           // Добавляем отступ сверху
-          const SizedBox(height: 12),
+          const SizedBox(height: 14),
           Expanded(
             child: _loading
                 ? const Center(child: CircularProgressIndicator())
@@ -526,9 +526,14 @@ class _ChatScreenState extends State<ChatScreen> {
                               ),
                               decoration: BoxDecoration(
                                 color: isMine 
-                                    ? const Color(0xFF7474d6)
-                                    : const Color(0xFF474747),
-                                borderRadius: BorderRadius.circular(12),
+                                  ? const Color(0xFF7474d6)
+                                  : const Color(0xFF474747),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: const Radius.circular(14),
+                                  topRight: const Radius.circular(14),
+                                  bottomLeft: const Radius.circular(14),
+                                  bottomRight: const Radius.circular(0),
+                                ),
                               ),
                               child: Row(
                                 mainAxisSize: MainAxisSize.min,
@@ -550,7 +555,7 @@ class _ChatScreenState extends State<ChatScreen> {
                                       _formatTime(msg['timestamp']),
                                       style: const TextStyle(
                                         fontSize: 10,
-                                        color: Colors.white60,
+                                        color: Colors.white70,
                                         height: 1.0,
                                       ),
                                     ),
@@ -562,12 +567,13 @@ class _ChatScreenState extends State<ChatScreen> {
                         },
                       ),
           ),
+          const SizedBox(height: 14),
           Padding(
             padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
             child: Container(
               decoration: BoxDecoration(
                 color: const Color(0xFF2f2f2f),
-                borderRadius: BorderRadius.circular(8),
+                borderRadius: BorderRadius.circular(16),
               ),
               child: Row(
                 children: [
